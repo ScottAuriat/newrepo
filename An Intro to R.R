489d4bc2eb5a1dd5d01fa7d7b7cc8ae7116145a4
Scott Auriat <scottauriat@gmail.com>
2022-10-28 16:24:39 -0600

    now

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,6 +1,8 @@
 # I am using https://intro2r.com/use_git.html
-x <- seq(1:100)
-x <- matrix(x,nrow = 10)
-(x%*%x)^10
+my_mat <- matrix(1:100,nrow = 10,byrow = T)
+my_array <- array(1:100,dim = c(10,10,2))
+list_1 <- list(c("this","that","why"),2,2,matrix(rep(0,100),nrow = 10),
+               array(1:100,dim = c(5,5,4)))
+list_2 <- list(list_1,seq(1:100))
+list_2[2]
 
-# This That 
