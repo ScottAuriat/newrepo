a7b767a57d4d2b63f05d5d98327d68f531ed6657
Scott Auriat <scottauriat@gmail.com>
2022-10-31 15:36:34 -0600

    Oct 31/22

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,6 +1,6 @@
 # I am using https://intro2r.com/use_git.html
-char_1 <- c("You","are not me","this")
-vec_1 <- rep(0,10)
-mat_1 <- diag(10)
-array_1 <- array(1:100,dim = c(5,5,4))
-list_1 <- list(char_1,vec_1,mat_1,array_1)
+# to start over use rm(list = ls())
+x <- 1:100
+y <- -50 + 3*x + rnorm(100,0,55)
+plot(y,x)
+summary(lm(y~x))
