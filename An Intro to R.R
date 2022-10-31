59cae3f56b0331ce2aaf032d829d27d1fcdf1a53
Scott Auriat <scottauriat@gmail.com>
2022-10-31 13:32:46 -0600

    Oct 31/22

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,6 +1,2 @@
 # I am using https://intro2r.com/use_git.html
-char_1 <- c("This","OK this is funny, but how many lines can you write and keep it going and going?")
-num_1 <- 1:100
-mat_1 <- diag(5)
-array_1 <- array(1:1000,dim = c(5,5,70))
-list_1 <- list(char_1,num_1,mat_1,array_1)
+
