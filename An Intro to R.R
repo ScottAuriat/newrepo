f9443960c0fcafd0f4fa4680d713b0cf41b0d8b2
Scott Auriat <scottauriat@gmail.com>
2022-10-31 13:32:12 -0600

    Oct 31/22 (Just messing around at this point)

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,8 +1,6 @@
 # I am using https://intro2r.com/use_git.html
-my_mat <- matrix(1:100,nrow = 10,byrow = T)
-my_array <- array(1:100,dim = c(10,10,2))
-list_1 <- list(c("this","that","why"),2,2,matrix(rep(0,100),nrow = 10),
-               array(1:100,dim = c(5,5,4)))
-list_2 <- list(list_1,seq(1:100))
-list_2[2]
-
+char_1 <- c("This","OK this is funny, but how many lines can you write and keep it going and going?")
+num_1 <- 1:100
+mat_1 <- diag(5)
+array_1 <- array(1:1000,dim = c(5,5,70))
+list_1 <- list(char_1,num_1,mat_1,array_1)
