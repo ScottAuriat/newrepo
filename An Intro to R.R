657e7dde2733b8226eca76cac6457a3d57634383
Scott Auriat <scottauriat@gmail.com>
2022-10-31 13:49:18 -0600

    Oct 31/22 (I created an list)

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,2 +1,6 @@
 # I am using https://intro2r.com/use_git.html
-
+char_1 <- c("You","are not me","this")
+vec_1 <- rep(0,10)
+mat_1 <- diag(10)
+array_1 <- array(1:100,dim = c(5,5,4))
+list_1 <- list(char_1,vec_1,mat_1,array_1)
