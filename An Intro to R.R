dc8332dc658e413c7fcdd7b8e1a12197c5b5968f
Scott Auriat <scottauriat@gmail.com>
2022-11-01 15:00:06 -0600

    Nov 1/2022

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,6 +1,15 @@
 # I am using https://intro2r.com/use_git.html
 # to start over use rm(list = ls())
+library(ggplot2)
 
 X512 <- read_excel("D:/Dropbox/512.xlsx")
+dat <- X512
+dat <- dat[1:361,]
 
+dat$Fid <- factor(dat$id)
+dat$Ft <- factor(dat$t)
+dat$Feu1 <- factor(dat$eu1)
 
+ggplot()+geom_point()+geom_point(aes(lngdp,`growth rate`),data = dat)+
+  geom_smooth(aes(lngdp,`growth rate`,colour = Feu1),method = "lm",se=F,data = dat)+
+  facet_wrap(~ `eu vs non`)
--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 This is just a test repo, I am working on https://intro2r.com/use_git.html
 
 # for now this isn't anything interesting, but someday 
-
-x <- 2
-y <- 2*x
-
+This is a subsection 
+# I am going to add this as well
