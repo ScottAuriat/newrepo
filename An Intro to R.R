f9eb8b5bb8a0afacde42c163e31c8cee04d1876a
Scott Auriat <scottauriat@gmail.com>
2022-11-01 14:00:53 -0600

    Nov 1/22

--- An Intro to R.R
+++ An Intro to R.R
@@ -1,6 +1,6 @@
 # I am using https://intro2r.com/use_git.html
 # to start over use rm(list = ls())
-x <- 1:100
-y <- -50 + 3*x + rnorm(100,0,55)
-plot(y,x)
-summary(lm(y~x))
+
+X512 <- read_excel("D:/Dropbox/512.xlsx")
+
+
