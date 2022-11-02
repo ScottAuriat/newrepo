d6247bc6ca9765a49dbf456b0bb608fdd62ce367
Scott Auriat <scottauriat@gmail.com>
2022-11-02 12:53:49 -0600

    Nov 2/22

--- An Intro to R.R
+++ An Intro to R.R
@@ -10,6 +10,7 @@ dat$Fid <- factor(dat$id)
 dat$Ft <- factor(dat$t)
 dat$Feu1 <- factor(dat$eu1)
 
-ggplot()+geom_point()+geom_point(aes(lngdp,`growth rate`),data = dat)+
-  geom_smooth(aes(lngdp,`growth rate`,colour = Feu1),method = "lm",se=F,data = dat)+
-  facet_wrap(~ `eu vs non`)
+ggplot() +geom_point(aes(lngdp,`growth rate`,shape = Ft),data= dat)+
+  theme_bw()+
+  facet_grid(~eu1)+
+  geom_smooth(aes(lngdp,`growth rate`),data=dat, method = "lm",se=F)
