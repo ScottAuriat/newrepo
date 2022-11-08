add75544ee678417ad86f0eb8be822a3a37b54e2
Scott Auriat <scottauriat@gmail.com>
2022-11-08 13:33:02 -0700

    Nov 8/2022

--- Tidy Finance.R
+++ Tidy Finance.R
@@ -2,5 +2,15 @@
 # packages ----
 library(tidyverse)
 library(tidyquant)
+library(quantmod)
 
 # Chap 1 ----
+prices <- tq_get("AAPL",from = "2000-01-10" )
+
+prices |> ggplot(aes(date,adjusted))+geom_line()+
+  labs(x= "",y="", title = "Apple since 2000")
+
+rerturns <- prices |>
+  arrange(date)|>
+  mutate(r = adjusted/lag(adjusted) - 1) |>
+  select(symbol,date,r)
