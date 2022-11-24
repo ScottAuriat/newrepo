39b360f5a7ee52585057d6d9aba4d0c6b7b03b83
Scott Auriat <scottauriat@gmail.com>
2022-11-24 15:38:10 -0700

    Nov 24/2022

--- Tidy Finance.R
+++ Tidy Finance.R
@@ -4,21 +4,16 @@ library(tidyverse)
 library(tidyquant)
 library(quantmod)
 # Chap 1 ----
-DOW_ticker <- tq_index("DOW")
-DOW_prices <- tq_get(DOW_ticker, from = "2000-01-01")
+ETH <- tq_get("ETH-USD")
+ETH |>
+  ggplot(aes(date,adjusted))+geom_line(size=1.2,col= "blue")+
+  labs(x="",y="",title = "Eth price since inception")+
+  theme_bw()+geom_point(size=1, col = "red")
 
-volume <- DOW_prices |>
-  group_by(date) |>
-  summarize(volume = sum(volume*close/1e9))
+ETHr <- ETH |>
+  arrange(date)|>
+  mutate(r = adjusted/lag(adjusted)-1) |>
+  select(symbol,date,r)
+ETHr <- drop_na(ETHr,r)
 
-volume |>
-  ggplot(aes(date,volume))+
-  geom_line()
 
-volume |>
-  ggplot(aes(log(volume),log(lag(volume))))+
-  geom_point()+geom_smooth(method="lm")+
-  labs(x="natural log of volume")+
-  theme_bw()
- version
- packageStatus()
