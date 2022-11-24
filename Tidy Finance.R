59098927adce153542094c7a3a9d71f73d729f04
Scott Auriat <scottauriat@gmail.com>
2022-11-24 16:30:26 -0700

    Test

--- Tidy Finance.R
+++ Tidy Finance.R
@@ -4,16 +4,27 @@ library(tidyverse)
 library(tidyquant)
 library(quantmod)
 # Chap 1 ----
-ETH <- tq_get("ETH-USD")
-ETH |>
-  ggplot(aes(date,adjusted))+geom_line(size=1.2,col= "blue")+
-  labs(x="",y="",title = "Eth price since inception")+
-  theme_bw()+geom_point(size=1, col = "red")
 
-ETHr <- ETH |>
-  arrange(date)|>
-  mutate(r = adjusted/lag(adjusted)-1) |>
+SNP500i <- tq_index("SP500")
+SNP500 <- tq_get(SNP500i)
+
+SNP500r <- SNP500 |>
+  group_by(symbol) |>
+  mutate(r = adjusted/lag(adjusted))|>
   select(symbol,date,r)
-ETHr <- drop_na(ETHr,r)
 
+SNP500r <- drop_na(SNP500r,r)
+SNP500r <- mutate(SNP500r,r = r*100)
 
+volume <- SNP500 |>
+  group_by(date) |>
+  summarize(volume = sum(volume*close/1e9))
+
+volume |>
+  ggplot(aes(date,volume))+geom_line()
+
+volume |>
+  ggplot(aes(log(volume),log(lag(volume))))+geom_point()+
+  labs(x="",y="",title = "Persistance of Volume")+
+  theme_bw()+
+  geom_abline(aes(intercept= 0,slope = 1))
